6734392363d8bf257e18a8ddc0cca535e1a36db1
Tirath Pathak <pathaktirath2000@gmail.com>
2025-11-15 18:13:34 +0530

    Fixed the merge code issue

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,6 @@ import helmet from 'helmet';
 import { limiter } from '@/lib/express_rate_limiter';
 import routes from '@/routes/v1';
 import { connectToDB } from '@/lib/mongoose';
-<<<<<<< HEAD
-
-const app = express();
-
-=======
 import { logger } from '@/lib/winston';
 
 const app = express();
@@ -19,18 +14,13 @@ const app = express();
 /**
  * When testing locally, the origin will be undefined, hence !origin.
  */
->>>>>>> d2b07a0c6b9b301becc98a27afa815abcd298f9f
 // configure CORS
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     if (
       config.NODE_ENV === 'development' ||
-<<<<<<< HEAD
-      config.WHITELISTED_PORTS?.includes(origin as string)
-=======
       config.WHITELISTED_URL?.includes(origin as string) ||
       !origin 
->>>>>>> d2b07a0c6b9b301becc98a27afa815abcd298f9f
     ) {
       callback(null, true);
     } else {
@@ -62,19 +52,6 @@ app.use(helmet());
 app.use(limiter);
 
 (async () => {
-<<<<<<< HEAD
-  // first-route
-  try {
-
-    await connectToDB()
-
-    app.use('/api', routes);
-    app.listen(config.PORT, () => {
-      console.log(`Listening on PORT:- ${config.PORT}`);
-    });
-  } catch (error) {
-    console.log(`An unexpected error occured:- ${error}`);
-=======
   try {
     await connectToDB();
 
@@ -84,27 +61,12 @@ app.use(limiter);
     });
   } catch (error) {
     logger.error(`An unexpected error occured:- ${error}`);
->>>>>>> d2b07a0c6b9b301becc98a27afa815abcd298f9f
     if (config.NODE_ENV === 'production') {
       process.exit(1); // Immediately exit in case of failure on production
     }
   }
 })();
 
-<<<<<<< HEAD
-
-const handleServerShutDown = async () => {
-  try {
-    console.log('Server shutdown');
-    process.exit(0)
-  } catch (error) {
-    console.log('Error in server shutdown')
-  }
-}
-
-process.on('SIGTERM', handleServerShutDown); //process stopped like container shutdown etc
-process.on('SIGINT', handleServerShutDown); // process interrupted like ctrl + c  
-=======
 const handleServerShutDown = async () => {
   try {
     logger.warn('Server shutdown');
@@ -116,4 +78,3 @@ const handleServerShutDown = async () => {
 
 process.on('SIGTERM', handleServerShutDown); //process stopped like container shutdown etc
 process.on('SIGINT', handleServerShutDown); // process interrupted like ctrl + c
->>>>>>> d2b07a0c6b9b301becc98a27afa815abcd298f9f
